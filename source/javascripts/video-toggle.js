eec84512af378614bf1687123564aec6697d1cc9
conchan <hello.cchan@gmail.com>
2014-07-21 22:02:51 -0400

    Desktop layout for Landing page

---
+++ source/javascripts/video-toggle.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".video").click(function(e){
+    $(this).toggleClass("show-video");
+    $(this).find("iframe").attr("src", "http://player.vimeo.com/video/70699061?autoplay=1");
+    $('.media .caption').fadeIn("slow");
+  });
+});
