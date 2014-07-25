63c733713ebf8708df8333a5ca505cca6d162cc7
conchan <hello.cchan@gmail.com>
2014-07-25 16:14:39 -0400

    Desktop styles for Header and Nav in internal pages

--- source/javascripts/video-toggle.js
+++ source/javascripts/video-toggle.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $(".video").click(function(e){
     $(this).toggleClass("show-video");
     $(this).find("iframe").attr("src", "http://player.vimeo.com/video/70699061?autoplay=1");
-    $('.media .caption').fadeIn("slow");
+    $(".media .caption").fadeIn("slow");
   });
 });
