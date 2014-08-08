dec71ab3ab6b857ec3e06604e361268ed0f31455
Melissa Xie <me@melissaxie.com>
2014-08-08 13:50:06 -0400

    Don't minimize assets on build

--- config.rb
+++ config.rb
@@ -6,10 +6,6 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 configure :build do
-  activate :asset_hash
-  activate :minify_css
-  activate :minify_javascript
-  activate :relative_assets
 end
 
 configure :development do
