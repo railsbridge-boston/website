d41c9a2f538ef13444948a215189d13b5aacf805
Gabe Berke-Williams <gabebw@gabebw.com>
2015-05-06 10:15:14 -0400

    Remove ActiveRecord from rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 
 require "rspec/rails"
-require "shoulda/matchers"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 
@@ -19,5 +18,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 end
 
-ActiveRecord::Migration.maintain_test_schema!
 Capybara.javascript_driver = :webkit
