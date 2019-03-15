cdfa1cf42fd9a2da76fea82c4b213516ebc54604
jacklynhma <jacklynhma@gmail.com>
2019-03-14 22:36:36 -0400

    adds bridge troll

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,12 @@
 module EventsHelper
   def next_event
-    @next_event ||= Eventbrite::EventFinder.find(ENV.fetch("NEXT_EVENT_ID"))
+    OpenStruct.new(venue: ENV.fetch("VENUE_MAP_URL"), url: ENV.fetch("EVENT_URL"))
+    # @next_event ||= Eventbrite::EventFinder.find(ENV.fetch("NEXT_EVENT_ID"))
   end
 
   def next_event_venue
-    next_event.venue
+    OpenStruct.new(name: ENV.fetch("VENUE_NAME"), address: ENV.fetch("VENUE_ADDRESS"))
+    @next_event.venue
   end
 
   def show_registration?
