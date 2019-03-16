3438b353e31acb8e6ee722167c557c000c1ba5c5
jacklynhma <jacklynhma@gmail.com>
2019-03-16 13:39:28 -0400

    houndci adjustmetns

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,11 +1,15 @@
 module EventsHelper
   def next_event
-    OpenStruct.new(venue: ENV.fetch("VENUE_MAP_URL"), url: ENV.fetch("EVENT_URL"))
+    OpenStruct.new(venue: ENV.fetch("VENUE_MAP_URL"), 
+      url: ENV.fetch("EVENT_URL")
+    )
     # @next_event ||= Eventbrite::EventFinder.find(ENV.fetch("NEXT_EVENT_ID"))
   end
 
   def next_event_venue
-    OpenStruct.new(name: ENV.fetch("VENUE_NAME"), address: ENV.fetch("VENUE_ADDRESS"))
+    OpenStruct.new(name: ENV.fetch("VENUE_NAME"), 
+      address: ENV.fetch("VENUE_ADDRESS")
+    )
     # @next_event.venue
   end
 
