77a907976b3e6706d67a3d78de35c200b43de50e
jacklynhma <jacklynhma@gmail.com>
2019-03-16 14:26:14 -0400

    updates venue

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,18 +1,23 @@
 module EventsHelper
+  require 'pry'
   def next_event
     OpenStruct.new(
-      venue: ENV.fetch("VENUE_MAP_URL"),
+      venue: venue,
       url: ENV.fetch("EVENT_URL")
     )
     # @next_event ||= Eventbrite::EventFinder.find(ENV.fetch("NEXT_EVENT_ID"))
   end
 
   def next_event_venue
+
+    next_event.venue
+  end
+
+  def venue 
     OpenStruct.new(
       name: ENV.fetch("VENUE_NAME"),
       address: ENV.fetch("VENUE_ADDRESS")
     )
-    # next_event.venue
   end
 
   def show_registration?
